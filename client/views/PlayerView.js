1887bfb551a870845c41ba3dffe68ae085a15c32
MKS-students <mks.students@hackreactor.com>
2016-09-22 17:42:32 -0500

    Stylized

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -3,7 +3,7 @@ var PlayerView = Backbone.View.extend({
 
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
-  el: '<div />',
+  el: '<div class="player" />',
 
   initialize: function() {
     this.$el.on('ended', 'audio', function() {
@@ -19,8 +19,12 @@ var PlayerView = Backbone.View.extend({
 
   render: function() {
     this.$el.empty();
-    $('body').css('background-image', 'url("' + this.model.get('artwork') + '")');
+    $('body').css('background-image', 'url("' + (this.model ? this.model.get('artwork') : '') + '")');
     return this.$el.append(
+      $('<div class="song-info" />').append(
+        $('<span class="title"/>').text(this.model.get('title')),
+        $('<span class="artist" />').text(this.model.get('artist'))
+      ),
       $('<audio controls autoplay />').attr('src', this.model ? this.model.get('url') : '').on('ended', function() {
         this.model.ended();
       }.bind(this))
